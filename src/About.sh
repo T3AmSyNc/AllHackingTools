28249a108ead51460d5221e6ceea22bc59f1d8c3
Zain Gaur <155817661+T3AmSyNc@users.noreply.github.com>
2024-02-29 17:31:30 +0530

    About.sh

--- src/About.sh
+++ src/About.sh
@@ -1,17 +1,17 @@
 echo '
     ┌──────────────────────────────────────────────────────────────┐
-    │  	               ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	              │
-    │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		      │
-    │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		      │
+    │  	           ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	                               │
+    │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		                           │
+    │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		                           │
     │──────────────────────────────────────────────────────────────│
     │                                                              │
     │ >>   Hi, here you can see my telegram and github there       |
     |       will be questions you will write in telegram.          │
     │                                                              │
-    │ >>  Author   :  Misha Korzhik                                │
-    │     Github   :  https://github.com/mishakorzik               │
-    │     Telegram :  pseudonym @MishaKorzhikTelegram              │
-    |     Info     :  Trank for installing AllHackingTools         |
+    │ >>  Author   :  Team Sync Owners (AHMAD)                     │
+    │     Github   :  https://github.com/T3AmSyNc                  │
+    │     Telegram :  pseudonym @T3AmSyNc                          │
+    |     Info     :  Thanks for installing AllHackingTools        |
     │                                                              │
     └──────────────────────────────────────────────────────────────┘ 
 '|lolcat -a -d 10 -p 1.5 -s 40.0
