6ec5a2762f070c45d82fcc9b7dd948b17d1edf60
Zain Gaur <155817661+T3AmSyNc@users.noreply.github.com>
2024-02-29 17:10:24 +0530

    Logo.sh

--- .settings/debug/Logo.sh
+++ .settings/debug/Logo.sh
@@ -1,15 +1,15 @@
 echo '
 [+]
- | ██╗░░██╗░███▓▓╗░░▓▓███╗░██╗░░██╗██╗███╗░░██╗░██████╗░░░░░░░▓▓██████╗░█████╗░░▓▓▓██╗░██╗░░░░░
- | ██║░░██║██╔══█▓╗▓▓╔══██╗██║░██╔╝█▓║▓███╗░██║██╔════╝░░░░░░░╚══██╔══╝██╔══▓▓╗▓▓╔══██╗██║░░░░░
- | ███████║███▓▓▓█║██║░░╚═╝█████═╝░▓▒║▒▓╔▓█╗██║██║░░██╗░███▓▓╗░░░██║░░░██║░░▓▓║▓▓║░░██║██║░░░░░
- | ██╔══▓█║██╔══▓▓║▓█║░░██╗██╔═██╗░▓▒║▓▓║╚████║██║░░╚██╗╚════╝▒▒░██║░░░██║░░█▓║▓▓║░░██║██║░░░░░
- | ██║░░█▓║██║░░▓▓║╚█████╔╝██║░╚▓▓╗▓▓║▓▓║░╚███║╚██████╔╝░░░░▒▒▒░░██║░░░╚█████╔╝╚█████╔╝███████╗
- | ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ 
- |
-[+] Version  : AllHackingTools v2.7.4    -  Debug mode: true
-[+] Company  : FlyZero Official Copyring ©  -  ⁣ATTENTION! The author of this article is not respo-
-[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+ | ███████╗██╗   ██╗███╗   ██╗ ██████╗
+ | ██╔════╝╚██╗ ██╔╝████╗  ██║██╔════╝
+ | ███████╗ ╚████╔╝ ██╔██╗ ██║██║     
+ | ╚════██║  ╚██╔╝  ██║╚██╗██║██║     
+ | ███████║   ██║   ██║ ╚████║╚██████╗
+ | ╚══════╝   ╚═╝   ╚═╝  ╚═══╝ ╚═════╝
+ |                                 
+[+] Version  : AllHackingTools v2.0   -  Debug mode: true
+[+] Company  : TEAM SYNC AHMAD ©  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/T3AmSyNc-  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : KASHU GAUR (AHMAD YK)  -  All materials are provided for educational purposes only! 
 [+]——————-——-–—————-—-–—————-————-—–————-———-—–—————————-——–—————–-——[DEBUG]
 '|lolcat -p 3.0 -a -d 2 -s 20.0
