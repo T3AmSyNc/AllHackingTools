8efe2483e3679155028bc41aa52e291abf53db62
Zain Gaur <155817661+T3AmSyNc@users.noreply.github.com>
2024-02-29 17:32:38 +0530

    CheckVersion.py

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -41,7 +41,7 @@ version = '2.7.4'
 
 def ver_check():
 	print(G + '[+]' + C + ' Checking the AllHackingTools for updates....', end='')
-	ver_url = 'https://raw.githubusercontent.com/mishakorzik/AllHackingTools/main/Castom/version.txt'
+	ver_url = 'https://raw.githubusercontent.com/T3AmSyNc/AllHackingTools/main/Castom/version.txt'
 	try:
 		ver_rqst = requests.get(ver_url)
 		ver_sc = ver_rqst.status_code
